d2041fb08a230b23ace4dd067e9618d0396f0214
micyo202 <micyo202@163.com>
2018-11-06 17:33:53 +0800

    添加组件示例模块

---
+++ YZFrame/Demo/DemoComponentViewController.h
@@ -0,0 +1,15 @@
+/************************************************************
+ Class    : DemoComponentViewController.h
+ Describe : 组件示例模块
+ Company  : Micyo
+ Author   : Yanzheng
+ Date     : 2018-11-01
+ Version  : 1.0
+ Declare  : Copyright © 2018 Yanzheng. All rights reserved.
+ ************************************************************/
+
+#import <UIKit/UIKit.h>
+
+@interface DemoComponentViewController : UIViewController
+
+@end
