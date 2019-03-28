4568d56e31d037761350d6c880c474d6303f8a15
micyo202 <micyo202@163.com>
2019-03-28 13:32:33 +0800

    项目更新优化

--- YZFrame/Supporting Files/main.m
+++ YZFrame/Supporting Files/main.m
@@ -1,10 +1,12 @@
-//
-//  main.m
-//  YZFrame
-//
-//  Created by cycfc on 2018/10/26.
-//  Copyright © 2018年 Yanzheng. All rights reserved.
-//
+/************************************************************
+ Class    : main.m
+ Describe : 长须入口
+ Company  : Micyo
+ Author   : Yanzheng
+ Date     : 2018-10-26
+ Version  : 1.0
+ Declare  : Copyright © 2018 Yanzheng. All rights reserved.
+ ************************************************************/
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
