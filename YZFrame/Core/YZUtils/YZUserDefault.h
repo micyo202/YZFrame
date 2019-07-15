354b8f9365478a41035bb8916136c3282c256b97
micyo202 <micyo202@163.com>
2019-07-15 09:13:17 +0800

    添加持久化用户对象工具类

--- YZFrame/Core/YZUtils/YZUserDefault.h
+++ YZFrame/Core/YZUtils/YZUserDefault.h
@@ -1,16 +1,22 @@
-//
-//  YZUserDefault.h
-//  YZFrame
-//
-//  Created by apple on 2019/7/14.
-//  Copyright © 2019 Yanzheng. All rights reserved.
-//
+/************************************************************
+ Class    : YZUserDefault.h
+ Describe : 轻量数据持久化对象方法
+ Company  : Micyo
+ Author   : Yanzheng
+ Date     : 2019-07-15
+ Version  : 1.0
+ Declare  : Copyright © 2019 Yanzheng. All rights reserved.
+ ************************************************************/
 
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface YZUserDefault : NSObject
+
++ (void)saveUserDefaultObject:(id)object key:(NSString *)key;
++ (id)getUserDefaultObjectByKey:(NSString *)key;
++ (void)removeObjectWithKey:(NSString *)key;
 
 @end
 
--- YZFrame/Core/YZUtils/YZUserDefault.m
+++ YZFrame/Core/YZUtils/YZUserDefault.m
@@ -1,13 +1,35 @@
-//
-//  YZUserDefault.m
-//  YZFrame
-//
-//  Created by apple on 2019/7/14.
-//  Copyright © 2019 Yanzheng. All rights reserved.
-//
+/************************************************************
+ Class    : YZUserDefault.m
+ Describe : 轻量数据持久化对象方法
+ Company  : Micyo
+ Author   : Yanzheng
+ Date     : 2019-07-15
+ Version  : 1.0
+ Declare  : Copyright © 2019 Yanzheng. All rights reserved.
+ ************************************************************/
 
 #import "YZUserDefault.h"
 
 @implementation YZUserDefault
+
++ (void)saveUserDefaultObject:(id)object key:(NSString *)key {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:object forKey:key];
+    [defaults synchronize];
+}
+
+
++ (id)getUserDefaultObjectByKey:(NSString *)key {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    id object = [defaults objectForKey:key];
+    return object;
+}
+
+
++ (void)removeObjectWithKey:(NSString *)key {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults removeObjectForKey:key];
+    [defaults synchronize];
+}
 
 @end
